85e00678c27f51469c6b5c95a5ab1598032ee355
abdiwakil.sh <abdiwakil.sh@gmail.com>
2026-07-03 18:43:12 +0300

    Add js-exercise4

---
+++ js-exercise4/main.js
@@ -0,0 +1,16 @@
+// Function
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(2, 3));
+console.log(add(5, 5));
+
+
+// Function Expression
+let add2 = function(a, b) {
+    return a + b;
+};
+
+console.log(add2(2, 3));
+console.log(add2(5, 5));

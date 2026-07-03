7e587cb6814842be8bc62f558505610e6c457a18
abdiwakil.sh <abdiwakil.sh@gmail.com>
2026-07-03 20:30:12 +0300

    Add Js-exercise9

---
+++ js-exercise9/main.js
@@ -0,0 +1,14 @@
+let car = {
+    make: "Honda",
+    model: "Civic",
+    year: 2022,
+    start: function() {
+        console.log("The car has started");
+    }
+};
+
+console.log(car.make);
+console.log(car.model);
+console.log(car.year);
+
+car.start();

2fe1b884119ff2f17964b1ab13b23a8e66e68f27
abdiwakil.sh <abdiwakil.sh@gmail.com>
2026-07-04 05:55:15 +0300

    Add js-exercise10

---
+++ js-exercise10/main.js
@@ -0,0 +1,8 @@
+let library = [
+    { title: "Atomic Habits", author: "James Clea", year: 2018 },
+    { title: "The Alchemist", author: "Paulo Coelho", year: 1988 },
+    { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 }
+];
+
+console.log(library[0].title); 
+console.log(library[1].author);

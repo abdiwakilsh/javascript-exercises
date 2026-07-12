7b1000c909111267d25e5b4479c9fed4c5936d2d
abdiwakil.sh <abdiwakil.sh@gmail.com>
2026-07-12 15:15:26 +0300

    Add js-exercise15

--- js-exercise15/main.js
+++ js-exercise15/main.js
@@ -2,7 +2,7 @@ const people = [
     { name: "Alice", age: 25, city: "Wonderland" },
     { name: "John", age: 30, city: "New York" },
     { name: "Sarah", age: 22, city: "London" },
-    { name: "Ahmed", age: 28, city: "Dubai" }
+    { name: "Ahmed", age: 35, city: "Dubai" }
 ];
 
 for (const person of people) {

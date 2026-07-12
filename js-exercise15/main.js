d7974f49d60c8e7bf0b4afdbf53063372dc0c1ad
Abdiwakil <abdiwakil.sh@gmail.com>
2026-07-12 15:21:15 +0300

    Add js-exercise15
    Add js-exercise15

---
+++ js-exercise15/main.js
@@ -0,0 +1,13 @@
+const people = [
+    { name: "Alice", age: 25, city: "Wonderland" },
+    { name: "John", age: 30, city: "New York" },
+    { name: "Sarah", age: 22, city: "London" },
+    { name: "Ahmed", age: 35, city: "Dubai" }
+];
+
+for (const person of people) {
+    for (const key in person) {
+        console.log(key + ": " + person[key]);
+    }
+    console.log("----------------");
+}

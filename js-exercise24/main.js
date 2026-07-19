ffb4afe9773d540ebd22f960057677c0f115ea1d
abdiwakil.sh <abdiwakil.sh@gmail.com>
2026-07-19 11:38:02 +0300

    Add js-exercise24

--- js-exercise24/main.js
+++ js-exercise24/main.js
@@ -2,5 +2,5 @@ function calculateArea(width, height = width) {
     return width * height;
 }
 
-console.log(calculateArea(5));    // Output: 25
-console.log(calculateArea(5, 10)); // Output: 50
+console.log(calculateArea(5));  
+console.log(calculateArea(5, 10));

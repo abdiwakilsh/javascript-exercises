36e70c1dbf80dd7664aaf40c470f76dc6693dca8
abdiwakil.sh <abdiwakil.sh@gmail.com>
2026-07-21 06:32:53 +0300

    Add js-exercise27

---
+++ js-exercise27/main.js
@@ -0,0 +1,21 @@
+function fetchDataWithPromise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const success = true;
+
+            if (success) {
+                resolve("Data fetched successfully!");
+            } else {
+                reject("Failed to fetch data.");
+            }
+        }, 2000);
+    });
+}
+
+fetchDataWithPromise()
+    .then((message) => {
+        console.log(message);
+    })
+    .catch((error) => {
+        console.log(error);
+    });

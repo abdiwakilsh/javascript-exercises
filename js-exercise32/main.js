7c93e3d43fbe65fcd21f88a7ca1c5b59040a3aeb
abdiwakil.sh <abdiwakil.sh@gmail.com>
2026-07-25 11:04:28 +0300

    Add js-exercise32

---
+++ js-exercise32/main.js
@@ -0,0 +1,11 @@
+
+const heading = document.querySelector("#title");
+const button = document.querySelector("button");
+
+console.log(heading);
+console.log(button);
+
+
+const paragraphs = document.querySelectorAll(".text");
+
+console.log(paragraphs);

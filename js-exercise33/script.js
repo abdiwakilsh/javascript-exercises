bc7f3eccd40c5ccea3a9e0330c711be46cea2bfe
abdiwakil.sh <abdiwakil.sh@gmail.com>
2026-07-25 11:20:30 +0300

    Add js-exercise33

---
+++ js-exercise33/script.js
@@ -0,0 +1,5 @@
+function changeContent() {
+    document.getElementById("heading").textContent = "Hello Students!";
+    document.getElementById("paragraph").textContent = "The text has been changed.";
+    document.getElementById("content").innerHTML = "<h3>New HTML Content</h3>";
+}
